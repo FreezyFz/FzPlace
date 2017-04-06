87a5482095010c8f404b5753cd98ac45a5d5f09f
FreezyFz <b12565b@Yahoo.com.tw>
2017-04-06 23:27:02 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'html');
+
+app.use('/pic', express.static(__dirname + '/pic'));
+app.use('/css', express.static(__dirname + '/css'));
+
+app.use('/', function (req, res, next) {
+	console.log('Request Url:' + req.url);
+	next();
+});
+
+app.get('/mine', function(req, res) {
+	res.render('mine');
+});
+
+app.get('/page2', function(req, res) {
+	res.render('page2');
+});
+
+app.get('/page3', function(req, res) {
+	res.render('page3');
+});
+
+app.get('/page4', function(req, res) {
+	res.render('page4');
+});
+
+app.get('/page5', function(req, res) {
+	res.render('page5');
+});
+
+
+app.listen(port);
