8c6c4d51acca8c954711c63222a62a5dc1c887c1
FreezyFz <b12565b@Yahoo.com.tw>
2017-04-12 22:54:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var routers = require('./routers');
 var app = express();
-
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use('/pic', express.static(__dirname + '/pic'));
-app.use('/css', express.static(__dirname + '/css'));
-
-app.use('/', function (req, res, next) {
-	console.log('Request Url:' + req.url);
-	next();
-});
-
-app.get('/', function(req, res) {
-	res.render('mine');
-});
-
-app.get('/page2', function(req, res) {
-	res.render('page2');
-});
-
-app.get('/page3', function(req, res) {
-	res.render('page3');
-});
-
-app.get('/page4', function(req, res) {
-	res.render('page4');
-});
-
-app.get('/page5', function(req, res) {
-	res.render('page5');
-});
-
+app.use('/', routers);
 
 app.listen(port);
